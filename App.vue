5bded3b631515ac0d714f5611eb3de32c645769a
Eduardo Ugolini <edu.ugolini2@gmail.com>
2018-06-17 11:39:46 -0300

    Iniciado projeto usando o vue-native

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <root>
+    <app-navigation></app-navigation>
+  </root>
+</template>
+ 
+<script>
+
+import Vue from "vue-native-core";
+import { StackNavigator } from "vue-native-router";
+import { Root, VueNativeBase } from "native-base";
+import HomeComponent from './src/components/HomeComponent'
+
+Vue.use(VueNativeBase);
+
+const AppNavigation = StackNavigator(
+  {
+    Home: HomeComponent,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default {
+    components: { 
+      Root,
+      AppNavigation 
+    }
+}
+</script>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,100 @@
+<template>
+  <nb-container>
+
+    <nb-body class="body">
+      <nb-icon class="icon" name="person" :style="{fontSize: 70}"></nb-icon>
+
+      <nb-form>
+          <nb-item floatingLabel class="email-input">
+            <nb-label>E-mail</nb-label>
+            <nb-input />
+          </nb-item>
+          <nb-item floatingLabel class="password-input">
+            <nb-label>Senha</nb-label>
+            <nb-input />
+          </nb-item>
+      </nb-form>
+
+      <view class="forgot-password-container">
+        <nb-text :style="{fontSize: 12, fontColor: '#333'}">Esqueci minha senha</nb-text>
+      </view>
+
+      <view class="center-container">
+        <nb-button>
+          <nb-text>Login</nb-text>
+        </nb-button>
+      </view>
+      
+      <nb-row class="horizontal-center-container">
+        <nb-icon class="icon" name="logo-facebook" :style="{color: '#3B579D'}"></nb-icon>
+        <nb-icon class="icon" name="logo-googleplus" :style="{color: '#DD5044'}"></nb-icon>
+        <nb-icon class="icon" name="logo-twitter" :style="{color: '#55ACEE'}"></nb-icon>
+      </nb-row>
+    </nb-body>
+
+    <nb-footer class="footer">
+      <nb-text>Criar nova conta</nb-text>
+    </nb-footer>
+  </nb-container>
+</template>
+ 
+<script>
+
+  export default {
+    name: 'homeComponent',
+    data: () => {
+      return {
+        emailInput: '',
+        passwordInput: ''
+      }
+    }
+  }
+</script>
+
+
+<style>
+.container {
+  /* background-image: '../assets/Homebackground.jpg'; */
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+
+.body {
+  width: 100%;
+  margin-top: 15%;
+}
+
+.footer {
+  border-color: transparent;
+  background-color: transparent;
+  height: 35px;
+}
+
+.email-input,
+.password-input {
+  /* border-color: gray;
+  border-width: 1;
+  background-color: #fff;
+  height: 40px; */
+  width: 80%;
+}
+
+.forgot-password-container {
+  margin-top: 20px;  
+}
+
+.center-container {
+  margin-top: 40px;  
+}
+
+.horizontal-center-container {
+  margin-top: 20px;
+}
+
+.icon {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+</style>
