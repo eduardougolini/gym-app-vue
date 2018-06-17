c4c8ce1eb7cc2450b8f32c37c080cb8613114b58
Eduardo Ugolini <edu.ugolini2@gmail.com>
2018-06-17 14:11:42 -0300

    Movido conteúdo do HomeComponent para o LoginComponent

--- App.vue
+++ App.vue
@@ -9,6 +9,7 @@
 import Vue from "vue-native-core";
 import { StackNavigator } from "vue-native-router";
 import { Root, VueNativeBase } from "native-base";
+import LoginComponent from './src/components/LoginComponent';
 import HomeComponent from './src/components/HomeComponent';
 import SignUpComponent from './src/components/SignUpComponent';
 import ForgotPasswordComponent from './src/components/ForgotPasswordComponent';
@@ -17,12 +18,13 @@ Vue.use(VueNativeBase);
 
 const AppNavigation = StackNavigator(
   {
+    Login: LoginComponent,
     Home: HomeComponent,
     SignUp: SignUpComponent,
     ForgotPassword: ForgotPasswordComponent
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Login',
   }
 );
 
