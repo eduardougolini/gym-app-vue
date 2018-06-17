8508ff8d701215f040503c2ca26e13095c989757
Eduardo Ugolini <edu.ugolini2@gmail.com>
2018-06-17 19:33:00 -0300

    Adicionado código para tentar corrigir problemas no android

--- App.vue
+++ App.vue
@@ -29,9 +29,25 @@ const AppNavigation = StackNavigator(
 );
 
 export default {
-    components: { 
-      Root,
-      AppNavigation 
+  components: { 
+    Root,
+    AppNavigation 
+  },
+  created: function() {
+    this.loadFonts();
+  },
+  methods: {
+    loadFonts: async function() {
+      try {
+        await Expo.Font.loadAsync({
+          Roboto: require("native-base/Fonts/Roboto.ttf"),
+          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+          Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+        });
+      } catch (error) {
+        console.log("some error occured", error);
+      }
     }
+  }
 }
 </script>
