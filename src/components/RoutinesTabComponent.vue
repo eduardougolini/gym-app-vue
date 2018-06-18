76b74de24d9d225b87b22ca7208d9eccade45326
Vinicius Schulte <schultev2@gmail.com>
2018-06-17 23:16:11 -0300

    Adicionado segundo card junto com sua imagem

--- src/components/RoutinesTabComponent.vue
+++ src/components/RoutinesTabComponent.vue
@@ -19,6 +19,24 @@
                         class="progress"></nb-row>
                 </nb-view>
             </nb-card>
+            <nb-card :style="{width: 120, height: 150}">
+                <nb-card-item>
+                    <nb-left>
+                        <nb-body>
+                            <nb-text>Rotina 2</nb-text>
+                        </nb-body>
+                    </nb-left>
+                </nb-card-item> 
+
+                <nb-card-item cardBody class="card">
+                    <image :source="cardImage2" class="card-item-image"/>
+                </nb-card-item>
+                <nb-view class="background">
+                    <nb-row
+                        v-bind:width="secondRoutine"
+                        class="progress"></nb-row>
+                </nb-view>
+            </nb-card>
         </nb-content>
       </nb-container>
 </template>
@@ -39,23 +57,28 @@
   background-color: gray;
   position: absolute;
   bottom: 0;
+  border-radius: 2px;
 }
 
 .progress {
   background-color: blue;
   height: 10;
   position: absolute;
+  border-radius: 2px;
 }
 </style>    
 
 <script>
 import cardImage from "../assets/arm-routine.jpg";
+import cardImage2 from "../assets/leg-routine.jpg";
 export default {
   name: "routinesTabComponent",
   data: function() {
     return {
       firstRoutine: "45%",
+      secondRoutine: "75%",
       cardImage,
+      cardImage2,
       stylesObj: {
         cardItemImage: {
           resizeMode: "cover"
