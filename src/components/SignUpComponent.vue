f0370ac715b189d3e891f09d294051725528d19a
Eduardo Ugolini <edu.ugolini2@gmail.com>
2018-07-01 19:24:09 -0300

    Adicionados mais campos necessários para o cadastro de usuário na tela de cadastro

--- src/components/SignUpComponent.vue
+++ src/components/SignUpComponent.vue
@@ -3,15 +3,23 @@
     <nb-form>
         <nb-item floatingLabel class="name-input">
           <nb-label>Nome Completo</nb-label>
-          <nb-input v-model="name" />
+          <nb-input v-model="userData.name" autoCapitalize="words"/>
         </nb-item>
         <nb-item floatingLabel class="email-input">
           <nb-label>E-mail</nb-label>
-          <nb-input v-model="email" />
+          <nb-input v-model="userData.email" autoCapitalize="none" keyboardType="email-address"/>
         </nb-item>
         <nb-item floatingLabel class="password-input">
           <nb-label>Senha</nb-label>
-          <nb-input v-model="password" />
+          <nb-input v-model="userData.password" autoCapitalize="none" secureTextEntry/>
+        </nb-item>
+        <nb-item floatingLabel class="weight-input">
+          <nb-label>Peso(kg)</nb-label>
+          <nb-input v-model="userData.weight" keyboardType="numeric"/>
+        </nb-item>
+        <nb-item floatingLabel class="height-input">
+          <nb-label>Altura(cm)</nb-label>
+          <nb-input v-model="userData.height" keyboardType="numeric"/>
         </nb-item>
         <nb-body class="signup-button-block">
           <nb-button :onPress="registerUser">
@@ -23,31 +31,47 @@
 </template>
 
 <script>
+
+import { Alert } from 'react-native';
+
 export default {
   name: 'signUpComponent',
+  props: {
+    navigation: {
+      type: Object
+    }
+  },
   data: () => {
     return {
-      name: '',
-      email: '',
-      password: ''
+      userData: {
+        name: '',
+        email: '',
+        password: '',
+        weight: '',
+        height: ''
+      }
     }
   },
   methods: {
     registerUser: function() {
+      console.log(this.userData)
       fetch('http://192.168.5.113:3000/register', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          name: this.name,
-          email: this.email,
-          password: this.password
-        })
+        body: JSON.stringify(this.userData)
       }).then((response) => response.json())
       .then((responseJson) => {
-        console.log(responseJson);
+        Alert.alert(
+          'Sucesso',
+          'Seu usuário foi criado com sucesso',
+          [
+            {text: 'OK', onPress: () => this.navigation.navigate('Home')},
+          ],
+          { cancelable: false }
+        );
       }).catch((error) => {
         console.log(error);
       });;
